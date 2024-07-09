fdffe70ea75affbae9e593dc28815e900234ccc4
zfate <zp.fate@qq.com>
2024-07-09 16:41:14 +0800

    feat: 5. 最长回文子串整理

--- LeetCode/5.最长回文子串/Solution5.swift
+++ LeetCode/5.最长回文子串/Solution5.swift
@@ -17,35 +17,34 @@ import Foundation
 extension Solution {
     
     func longestPalindrome(_ s: String) -> String {
-            if s.count < 2 {
-                return s
-            }
-            var start = 0
-            var end = 0
-            var array = s.map({ String.init($0)})
-            for i in 0 ..< s.count {
-                let len1 = expandCenter(array: array, left: i, right: i)
-                let len2 = expandCenter(array: array, left: i, right: i + 1)
-                let len = max(len1, len2)
-                if(len > end - start) {
-                    start = i - (len - 1) / 2
-                    end = i + len / 2
-                }
-            }
-            array.removeFirst(start)
-            let sub = array.prefix(end - start + 1).joined()
-            return sub
+        if s.count < 2 {
+            return s
         }
-        
-        func expandCenter(array: [String], left: Int, right: Int) -> Int {
-            var l = left
-            var r = right
-            while l >= 0 && r < array.count && array[l] == array[r] {
-                l -= 1
-                r += 1
+        var start = 0
+        var end = 0
+        var array = s.map({ String.init($0)})
+        for i in 0 ..< s.count {
+            let len1 = expandCenter(array: array, left: i, right: i)
+            let len2 = expandCenter(array: array, left: i, right: i + 1)
+            let len = max(len1, len2)
+            if(len > end - start) {
+                start = i - (len - 1) / 2
+                end = i + len / 2
             }
-            return r - l - 1
         }
+        array.removeFirst(start)
+        let sub = array.prefix(end - start + 1).joined()
+        return sub
+    }
+    
+    func expandCenter(array: [String], left: Int, right: Int) -> Int {
+        var l = left
+        var r = right
+        while l >= 0 && r < array.count && array[l] == array[r] {
+            l -= 1
+            r += 1
+        }
+        return r - l - 1
     }
 
 }
